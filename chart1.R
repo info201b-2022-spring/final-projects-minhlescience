8945266e43d3aad6517ba3996d86598c24e722e1
MINH LE <MLE21@UW.EDU>
2022-05-20 07:38:36 -0700

    Final

--- chart1.R
+++ chart1.R
@@ -7,18 +7,6 @@ library(dplyr)
 # Read Fire dataset from 2012 - 2015
 fire_2012_2015_df <- read.csv("data/Fires_2012_2015.csv")
 
-# What 10 fires had the maximum fire size during 2012-2015?
-top_10_biggest_fire_size <- fire_2012_2015_df %>%
-  top_n(10, wt = FIRE_SIZE) %>% 
-  arrange(-FIRE_SIZE) %>%
-  select(FIRE_NAME, STATE, FIRE_YEAR, FIRE_SIZE, FIRE_SIZE_CLASS)
-
-# Count the total fire size class during 2012-2015
-fire_size_class_count <- fire_2012_2015_df %>%
-  group_by(FIRE_SIZE_CLASS) %>%
-  count()
-colnames(fire_size_class_count) <- c("fire_size_class", "total_number") # Format column names
-
 # Count the total fire in each state during 2012-2015
 state_fire_count <- fire_2012_2015_df %>%
   group_by(STATE, FIRE_YEAR) %>%
--- chart3.R
+++ chart3.R
@@ -7,12 +7,17 @@ library(dplyr)
 # Read Fire dataset from 2012 - 2015
 fire_2012_2015_df <- read.csv("data/Fires_2012_2015.csv")
 
-# What 10 fires had the maximum fire size during 2012-2015?
-top_5_biggest_fire_size <- fire_2012_2015_df %>%
-  top_n(5, wt = FIRE_SIZE) %>% 
-  arrange(-FIRE_SIZE) %>%
-  select(FIRE_NAME, FIRE_SIZE)
+# Count the total causes of wildfires during 2012-2015
+cause_fire_count <- fire_2012_2015_df %>%
+  group_by(STAT_CAUSE_DESCR) %>%
+  count()
+colnames(cause_fire_count) <- c("cause", "number_of_fires")
 
-# Barplot
-ggplot(top_5_biggest_fire_size, aes(x=FIRE_NAME, y=FIRE_SIZE)) + 
-  geom_bar(stat = "identity")
+cause_fire_count %>%
+  ggplot( aes(x=cause, y=number_of_fires)) +
+  geom_bar(stat="identity", fill="#f68060", alpha=.6, width=.4) +
+  coord_flip() +
+  ggtitle("Causes of wildfires during 2012-2015") +
+  xlab("") +
+  ylab("Number of wildfires") +
+  theme_bw()
