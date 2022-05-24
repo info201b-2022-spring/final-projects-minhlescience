875fe816a23406cbf7d76116076f5a825da3b31a
Bowang Lan <bowanglan@Jeffreys-MacBook-Pro.local>
2022-05-24 15:04:08 -0700

    added fire size class charts

--- aggregate.R
+++ aggregate.R
@@ -45,6 +45,7 @@ state_with_max_fire_count = head(fires_by_states, 1)
 state_with_min_fire = tail(fires_by_states, 1)
 
 
+
 #################
 # Fires by year #
 #################
@@ -59,16 +60,46 @@ ggplot(fires_by_year, aes(x=FireYear, y=Count)) +
   geom_line() + 
   geom_point() + 
   scale_x_continuous(breaks=seq(from=1992, to=2015, by=1)) +
-  theme(panel.grid.minor = element_blank())
+  theme(panel.grid.minor = element_blank()) +
+  ggtitle('Total Fire Counts in US From 1992-2015')
 
 # Years that have the top 10 fire count in US from 1992 to 2015
 years_with_top_10_fire_count <- fires_by_year %>%
-  top_n(10, Count) %>%
-  arrange(-Count)
+  arrange(-Count) %>%
+  head(10)
 years_with_top_10_fire_count$FireYear = as.character(years_with_top_10_fire_count$FireYear)
 # Chart for the above: Bar Plot
 ggplot(years_with_top_10_fire_count, aes(x=reorder(FireYear, -Count), y=Count)) +
-  geom_bar(stat='identity')
+  geom_col(fill=3) +
+  geom_text(aes(label=Count), vjust=2, color='white') +
+  ggtitle('Top 10 Years with the Most Fire counts in US during 1992-2015') +
+  xlab('Year') +
+  ylab('Fire Count')
+
+
+#######
+# Fires by fire size
+#######
+
+fires_by_fire_size_class = fires %>%
+  group_by(FireSizeClass) %>%
+  count() %>%
+  rename(Count = n) %>%
+  arrange(-Count)
+ggplot(fires_by_fire_size_class, aes(x=FireSizeClass, y=Count)) +
+  geom_col() +
+  geom_text(aes(label=Count), vjust=-1, color='black') +
+  ggtitle('Fire Counts in US for Each Fire Size Class during 1992-2015') +
+  xlab('Fire Size Class')
+
+fires_by_fire_size_class_and_year = fires %>%
+  group_by(FireSizeClass, FireYear) %>%
+  count() %>%
+  rename(Count = n)
+ggplot(fires_by_fire_class_and_year, aes(x=FireYear, y=Count, group=FireSizeClass, color=FireSizeClass)) +
+  geom_line() +
+  geom_point() +
+  ggtitle('Fire Count Progression in US during 1992-2015 for Each Fire Size Class')
 
 
 # What fires had the maximum fire size during 1992-2015?
